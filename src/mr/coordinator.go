465ef0d3e87ad1a46fd65a0ad39a81c195e12156
Zuzanna Maria Tarkowska <144691443+Zuzanna-Tarkowska@users.noreply.github.com>
2025-11-28 17:01:38 +0100

    Refactor task handling and update TaskDistributor
    Refactor taskHandler to use taskCount for file selection and update TaskDistributor to include NReduce.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,42 +1,46 @@
 package mr
 
-import "log"
+import (
+	"log"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+var taskCount int = 2
+
 type Coordinator struct {
 	// Your definitions here.
-
+	NReduce int
 }
 
 // TODO Possibly make a psudo please exit task for workers
 // Your code here -- RPC handlers for the worker to call.
 func (c *Coordinator) TaskDistributor(args *TaskArgs, reply TaskReply) error {
 	reply.Task = taskHandler()
+	reply.NReduce = c.NReduce
 	return nil
 }
-func taskHandler() string {
-	for _, filename := range os.Args[2:] {
 
+// TODO end program
+func taskHandler() string {
+	filename := os.Args[taskCount]
+	if taskCount < len(os.Args[2:]) {
+		taskCount++
 	}
-	return "file.txt"
+	return filename
 }
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -50,10 +54,8 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
@@ -62,15 +64,13 @@ func (c *Coordinator) Done() bool {
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
 	// Your code here.
+	c.NReduce = nReduce
 
 	c.server()
 	return &c
