7fcc0a62165f58e74b999dde83eecdcd1a7fab29
Zuzanna Maria Tarkowska <144691443+Zuzanna-Tarkowska@users.noreply.github.com>
2025-11-28 17:04:01 +0100

    Update worker.go

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -19,40 +19,49 @@ func (a ByKey) Len() int           { return len(a) }
 func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
+//
 // Map functions return a slice of KeyValue.
+//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
+//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
+//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+//
 // main/mrworker.go calls this function.
+//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 	//TODO One way to get started is to modify mr/worker.go's Worker() to send an RPC to the coordinator asking for a task
 	//     	Then modify the coordinator to respond with the file name of an as-yet-unstarted map task.
 	//     	Then modify the worker to read that file and call the application Map function, as in mrsequential.go.
 	//		timeout after 10 sec
+	// 		Implement atomic writing
 	for {
 		// Your worker implementation here.
-		taskType, task, nReduce, taskID := taskCall()
-		if taskType == "map" {
-			mapTask(mapf, task, nReduce, taskID)
-		} else if taskType == "reduce" {
-			reduceTask(reducef, nReduce, taskID)
-		} else if taskType == "wait" {
+		taskType, task, ncount, taskID := taskCall()
+		switch taskType {
+		case MAP:
+			mapTask(mapf, task, ncount, taskID)
+		case REDUCE:
+			reduceTask(reducef, ncount, taskID)
+		case WAIT:
 			time.Sleep(300 * time.Millisecond)
-		} else if taskType == "exit" {
+		case EXIT:
 			return
+		default:
+			time.Sleep(300 * time.Millisecond)
 		}
-
 	}
 
 	// uncomment to send the Example RPC to the coordinator.
@@ -61,7 +70,6 @@ func Worker(mapf func(string, string) []KeyValue,
 
 }
 func reduceTask(reducef func(string, []string) string, NMap int, reduceID int) {
-
 	var intermediate []KeyValue
 	for i := 0; i < NMap; i++ {
 		filename := fmt.Sprintf("mr-%d-%d", i, reduceID)
@@ -78,10 +86,8 @@ func reduceTask(reducef func(string, []string) string, NMap int, reduceID int) {
 	}
 	sort.Sort(ByKey(intermediate))
 
-	//TODO rename
 	oname := fmt.Sprintf("mr-out-%d", reduceID)
-	ofile, _ := os.Create(oname)
-
+	ofile, _ := ioutil.TempFile("", fmt.Sprintf("tm-out-%d", reduceID))
 	i := 0
 	for i < len(intermediate) {
 		j := i + 1
@@ -95,33 +101,35 @@ func reduceTask(reducef func(string, []string) string, NMap int, reduceID int) {
 		output := reducef(intermediate[i].Key, values)
 
 		// this is the correct format for each line of Reduce output.
-		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
 
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
 		i = j
 	}
-
 	ofile.Close()
+	os.Rename(ofile.Name(), oname)
+	reduceDoneCall(reduceID)
 }
 
 func mapTask(mapf func(string, string) []KeyValue, task string, nReduce int, taskID int) {
-
 	file, _ := os.Open(task)
 	content, _ := ioutil.ReadAll(file)
 	defer file.Close()
 	kva := mapf(task, string(content))
 	intermediateFileCreator(kva, nReduce, taskID)
+	mapDoneCall(taskID, nReduce)
 }
 
 func intermediateFileCreator(kva []KeyValue, nReduce int, taskID int) {
-
 	enc := make([]*json.Encoder, nReduce)
-	files := make([]*os.File, nReduce)
+	tempFiles := make([]*os.File, nReduce)
+	outputFileNames := make([]string, nReduce)
 
 	for i := 0; i < nReduce; i++ {
-		fileName := fmt.Sprintf("mr-%d-%d", taskID, i)
-		file, _ := os.Create(fileName)
-		files[i] = file
-		enc[i] = json.NewEncoder(file)
+		tmp, _ := ioutil.TempFile("", fmt.Sprintf("tm-%d-%d", taskID, i))
+		tempFiles[i] = tmp
+		enc[i] = json.NewEncoder(tmp)
+		outputFileNames[i] = fmt.Sprintf("mr-%d-%d", taskID, i)
+
 	}
 
 	for _, kv := range kva {
@@ -129,27 +137,49 @@ func intermediateFileCreator(kva []KeyValue, nReduce int, taskID int) {
 		enc[bucket].Encode(kv)
 	}
 
-	for _, f := range files {
+	for i, f := range tempFiles {
 		f.Close()
+		os.Rename(f.Name(), outputFileNames[i])
+
 	}
-	//TODO notify coordinater of files
 
 }
 
+//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
+//
+
 func taskCall() (string, string, int, int) {
 	args := TaskArgs{}
 
 	reply := TaskReply{}
 
-	ok := call("Coordinator.server", &args, &reply)
+	ok := call("Coordinator.taskDistributor", &args, &reply)
 	if !ok {
-		return "exit", "", 0, 0
+		//TODO
 	}
-	//TODO terminate worker
-	return reply.TaskType, reply.Task, reply.NReduce, reply.TaskID
+	return reply.TaskType, reply.Task, reply.NCount, reply.TaskID
+}
+func mapDoneCall(mapID int, nReduce int) {
+	args := MapDoneArgs{}
+	args.MapID = mapID
+	reply := DoneReply{}
+	ok := call("Coordinator.mapTasksDone", &args, &reply)
+	if !ok {
+		//TODO
+	}
+
+}
+func reduceDoneCall(reduceID int) {
+	args := ReduceDoneArgs{}
+	args.ReduceID = reduceID
+	reply := DoneReply{}
+	ok := call("Coordinator.reduceTasksDone", &args, &reply)
+	if !ok {
+		//TODO
+	}
 }
 
 func CallExample() {
@@ -172,14 +202,15 @@ func CallExample() {
 		// reply.Y should be 100.
 		fmt.Printf("reply.Y %v\n", reply.Y)
 	} else {
-		//TODO use call = false to stop worker
 		fmt.Printf("call failed!\n")
 	}
 }
 
+//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
+//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
